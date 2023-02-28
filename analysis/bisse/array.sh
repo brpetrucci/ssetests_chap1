159438464bd0f2af1239bd96b09d4a4b8a313a8d
brpetrucci <petrucci@iastate.edu>
2023-02-28 13:22:17 -0600

    Modified slurm scripts. Fixed a typo in the bisse analysis.

--- analysis/bisse/array.sh
+++ analysis/bisse/array.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1 # 1 node per job
 #SBATCH --time=01:00:00 # just an hour--calling the array itself doesn't take long 
-#SBATCH --array=1-9 # 76 parameter combinations
+#SBATCH --array=1-9 # 76 parameter combinations, 9 at a time
 
 #SBATCH --output=output/bisse/jobs/array/job_%A_%a.out
 #SBATCH --error=output/bisse/jobs/array/job_%A_%a.err
@@ -15,4 +15,4 @@
 #SBATCH --mail-type=FAIL
 
 mkdir output/bisse/jobs/job_${SLURM_ARRAY_TASK_ID}
-sbatch --time=30-00:00:00 --array=1-100 --output="output/bisse/jobs/job_${SLURM_ARRAY_TASK_ID}/bisse_${SLURM_ARRAY_TASK_ID}_%A_%a.out" --error="output/bisse/jobs/job_${SLURM_ARRAY_TASK_ID}/bisse_${SLURM_ARRAY_TASK_ID}_%A_%a.err" --mail-user=petrucci@iastate.edu --mail-type=BEGIN --mail-type=END --mail-type=FAIL --job-name="${SLURM_ARRAY_TASK_ID}_array" --wrap="sh analysis/bisse/bisse.sh ${SLURM_ARRAY_TASK_ID}"
+sbatch --time=25:00:00 --array=1-100 --output="output/bisse/jobs/job_${SLURM_ARRAY_TASK_ID}/bisse_${SLURM_ARRAY_TASK_ID}_%A_%a.out" --error="output/bisse/jobs/job_${SLURM_ARRAY_TASK_ID}/bisse_${SLURM_ARRAY_TASK_ID}_%A_%a.err" --mail-user=petrucci@iastate.edu --mail-type=BEGIN --mail-type=END --mail-type=FAIL --job-name="${SLURM_ARRAY_TASK_ID}_array" --wrap="sh analysis/bisse/bisse.sh ${SLURM_ARRAY_TASK_ID}"
