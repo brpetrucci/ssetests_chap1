21dd4bfe654a3a803fe9c6d1cd1b766826771116
brpetrucci <petrucci@iastate.edu>
2023-07-05 20:09:10 -0500

    Added refs builder script.

---
+++ analysis/hisse/refs_builder.R
@@ -0,0 +1,59 @@
+modelComb <- c(1, 2, 3)
+parComb <- c(1, 2, 3, 4, 5, 6)
+traitComb <- c(1, 2, 3, 4)
+psiComb <- c(1, 2, 3)
+
+count <- 1
+for (m in modelComb) {
+  if (m == 1) {
+    for (p in parComb) {
+      for (r in psiComb) {
+        lines <- c(paste0("modelComb <- ", m),
+                   paste0("parComb <- ", p),
+                   paste0("traitComb <- 1"),
+                   paste0("psiComb <- ", r))
+        
+        fileConn <- file(paste0("refs_", count, ".Rev"))
+        writeLines(lines, fileConn)
+        close(fileConn)
+        
+        count <- count + 1
+      }
+    }
+  }
+  
+  else if (m == 2) {
+    for (p in parComb) {
+      for (t in traitComb) {
+        lines <- c(paste0("modelComb <- ", m),
+                   paste0("parComb <- ", p),
+                   paste0("traitComb <- ", t),
+                   paste0("psiComb <- 1"))
+        
+        fileConn <- file(paste0("refs_", count, ".Rev"))
+        writeLines(lines, fileConn)
+        close(fileConn)
+        
+        count <- count + 1
+      }
+    }
+  }
+  
+  else {
+    for (p in parComb) {
+      for (t in traitComb) {
+        for (r in psiComb) {
+          lines <- c(paste0("modelComb <- ", m),
+                     paste0("parComb <- ", p),
+                     paste0("traitComb <- ", t),
+                     paste0("psiComb <- ", r))
+          fileConn <- file(paste0("refs_", count, ".Rev"))
+          writeLines(lines, fileConn)
+          close(fileConn)
+          
+          count <- count + 1
+        }
+      }
+    }
+  }
+}
